4c477b27ae091e59af1ca3f7e8355bfa73359aec
AthiraLekshmiCV <athiralek@gmail.com>
2014-06-07 18:29:53 +0530

    Updated

--- ls -l.c
+++ ls -l.c
@@ -1,66 +1,57 @@
-/**************************
- Author:Athira Lekshmi C V
+/******************************
+Author:Athira Lekshmi C V
 Date:6-6-2014
-Implementing ls-l
-*************************/
-#include <sys/types.h>
+ls -l command implementation
+*****************************/
+#include <stdio.h>
+#include <dirent.h>
+#include<sys/types.h>
 #include<sys/stat.h>
-#include<stdio.h>
-#include<dirent.h>
+#include<unistd.h>
 #include<pwd.h>
 #include<grp.h>
 #include<time.h>
-#include<string.h>
 #include<stdlib.h>
-#define MAXLENGTH 64
-#define MAXTIME   300
-char file_type(mode_t);
-char* file_perms(mode_t);
-int main(int argc,char* argv[]){
-    DIR *dp;
-    struct dirent *dirp;
-    struct passwd *user;
-    struct group *grp;
-    struct tm *tmp;
-    struct stat filestat;
-    char buf[MAXLENGTH];
-    char timebuf[MAXTIME];
-    
-    if(argc!=2)
-         perror("usage:./a.out directory name\n");
-    
-    if((dp=opendir(argv[1]))==NULL)
-         perror("No such file or directory\n");
+int main(int argc,char * argv[])
+{
+    struct dirent *de;          // Pointer for directory entry
+    char *ptr;
+    struct stat buf;
+    struct passwd * usr;
+    struct  group *grp;
+    struct tm *tme;
+    char tmbuf[256];
 
-    while(dirp=(readdir(dp))!=NULL){
-            lstat(dirp->d_name,&filestat);
-            user = getpwuid(filestat.st_uid);
-            //grp = getgruid(filestat.st_gid);
-            tmp = localtime(&filestat.st_mtime);
-            strftime(timebuf,sizeof(timebuf),"%H:%M:%S",tmp);
-            printf("%d  %s %s %s\n",filestat.st_nlink,user->pw_name,timebuf,dirp->d_name);
-            printf("%s",file_perms(filestat.st_mode));
+    DIR *dr = opendir(argv[1]);   // opendir() returns a pointer of DIR type.
+
+    if (dr == NULL)            // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not opendir current directory" );
+        return 0;
     }
 
-    return 0;
-}
-char* file_perms(mode_t mode){
-        char f;
-        if(S_ISREG(mode)) f='-';
-        if(S_ISDIR(mode)) f='d';
-        char perms_buff[30];
-        sprintf(perms_buff,"%c%c%c%c%c%c%c%c%c%c",f,
-        mode & S_IRUSR ? 'r' : '-',
-        mode & S_IWUSR ? 'w' : '-',
-        mode & S_IXUSR ? 'x' : '-',
-        mode & S_IRGRP ? 'r' : '-',
-        mode & S_IWGRP ? 'w' : '-',
-        mode & S_IXGRP ? 'x' : '-',
-        mode & S_IROTH ? 'r' : '-',
-        mode & S_IWOTH ? 'w' : '-',
-        mode & S_IXOTH ? 'x' : '-');
-        return perms_buff;
+    usr=getpwuid(buf.st_uid);
+    grp=getgrgid(buf.st_gid);
 
+    while ((de = readdir(dr)) != NULL){
+	if(stat(argv[1],&buf)<0){
+	    perror("error");
+        }
+	tme=localtime(&buf.st_mtime);
+    strftime(tmbuf,sizeof(tmbuf),"%A %B %H:%M:%S",tme);
+    switch (buf.st_mode & S_IFMT) {
+        case S_IFDIR:  printf("d ");            break;
+        case S_IFREG:  printf("r ");            break;
+        default:       printf("- ");            break;
+    }
+    printf("%s ",usr->pw_name);
+    printf("%s ",grp->gr_name);
+    printf("%d ",buf.st_nlink);
+    printf("%s ",tmbuf);
+    printf("%s\n", de->d_name);
+    }
+    closedir(dr);
+    return 0;
 }
 
 
