5af15bb1953e5f85299cc5784d66a024f5cea843
Athira Lekshmi C V <athiralek@gmail.com>
2014-06-20 11:15:07 +0530

    new commit

---
+++ ls -R.c
@@ -0,0 +1,53 @@
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<dirent.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+int i;
+int count;
+void recursive_path(char * fname){
+	DIR  *dr;
+	char * temp;
+	char ** fpath;
+	struct dirent *dir;
+	struct  stat mystat;
+	printf("\n%s-\n", fname);
+	temp = fname + strlen(fname);
+	*temp = '/';
+	*++temp = 0;
+    if ((dr = opendir(fname)) == 0) 
+		perror("Can't open this directory");
+		  
+    (stat(fname, &mystat));
+	count = 0;
+	fpath = (char **)malloc(1000*sizeof(char *));
+	while ((dir = readdir(dr)) != NULL) {
+		if(strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) 
+		    continue;
+		
+		
+		printf("%s\n",dir->d_name);
+		strcpy(temp,dir->d_name);
+		if(stat(fname,&mystat)<0)
+			perror("Stat error\n");
+		if(S_ISDIR(mystat.st_mode)){
+			fpath[count]=(char*)malloc(strlen(fname)*sizeof(char*));
+			strcpy(fpath[count],fname);
+			count++;
+		}
+	}
+
+	closedir(dr);
+
+	for(i=0;i<count;i++){
+		recursive_path(fpath[i]);
+	}
+}
+	
+		
+int main(int argc,char * argv[]){
+	recursive_path(argv[1]);
+	return 0;
+}
